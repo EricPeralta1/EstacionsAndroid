70f7c9869cb4b876fae24232a1357c9b3b422518
UWU <orez.molins@gmail.com>
2024-10-23 20:22:52 +0200

    Setting introActivityLayout

---
+++ app/src/main/java/com/example/estacionsandroid/IntroActivity.kt
@@ -0,0 +1,13 @@
+package com.example.estacionsandroid
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.introlayout)
+    }
+
+
+}
--- app/src/main/java/com/example/estacionsandroid/introActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.estacionsandroid
-
-import androidx.appcompat.app.AppCompatActivity
-
-class introActivity : AppCompatActivity() {
-}
