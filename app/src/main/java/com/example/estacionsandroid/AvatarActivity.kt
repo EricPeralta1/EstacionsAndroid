6a4aaf7b9ace69c37e7f56327588fc81ccb564c2
UWU <orez.molins@gmail.com>
2024-10-23 20:00:48 +0200

    Added AvatarCloning to the image on top of the play button. Setup clicking interactions for all avatar selection Setup animation when an avatar is selected Setup clicking interaction to the whole LinearLayout on the right side [id:confirmAvatarLayout] to start the next activity and send the avatars name and Id

--- app/src/main/java/com/example/estacionsandroid/AvatarActivity.kt
+++ app/src/main/java/com/example/estacionsandroid/AvatarActivity.kt
@@ -1,12 +1,19 @@
 package com.example.estacionsandroid
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.widget.AdapterView
 import android.widget.GridView
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 
 class AvatarActivity : AppCompatActivity() {
     private lateinit var adapter : AvatarAdapter
     private lateinit var lstAvatar : GridView
+    private lateinit var confirmAvatarLayout: LinearLayout
+    private lateinit var avatarCopy: ImageView
 
     private var avatarlist = mutableListOf(
         Avatar("batman",1),
@@ -30,13 +37,42 @@ class AvatarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.avatarlayout)
+        avatarCopy = findViewById(R.id.avatarCopy)
+        confirmAvatarLayout = findViewById(R.id.confirmAvatarLayout)
         updateAdapter()
-
+        setupAvatarClickListener()
     }
+
+
+
     private fun updateAdapter(){
         lstAvatar = findViewById(R.id.AvatarView)
         adapter = AvatarAdapter(this,R.layout.avatarimage,avatarlist)
         lstAvatar.adapter = adapter
         adapter.notifyDataSetChanged()
+    }
+
+    private fun setupAvatarClickListener() {
+        lstAvatar.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+            val avatarId = resources.getIdentifier(avatarlist[position].name, "drawable", packageName)
+            avatarCopy.setImageResource(avatarId)
+
+            val selectedAvatar= avatarlist[position]
+
+            val pulseAnimation = AnimationUtils.loadAnimation(this, R.anim.pulse_animation)
+            confirmAvatarLayout.startAnimation(pulseAnimation)
+            setupStartGameClickListener(selectedAvatar.id, selectedAvatar.name)
+        }
+    }
+
+    private fun setupStartGameClickListener(avatarId: Int, avatarName: String) {
+        confirmAvatarLayout.setOnClickListener{
+            val intent= Intent(this, introActivity::class.java)
+            intent.putExtra("Avatar_ID", avatarId)
+            intent.putExtra("Avatar_Name", avatarName)
+            startActivity(intent)
+
+            finish()
+        }
     }
 }
---
+++ app/src/main/java/com/example/estacionsandroid/introActivity.kt
@@ -0,0 +1,6 @@
+package com.example.estacionsandroid
+
+import androidx.appcompat.app.AppCompatActivity
+
+class introActivity : AppCompatActivity() {
+}
